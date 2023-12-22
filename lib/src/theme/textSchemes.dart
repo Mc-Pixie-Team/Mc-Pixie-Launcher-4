e38552e4d0e6219fa75507b066d2f506da51762f
joshiGaming <88989206+joshiGaming@users.noreply.github.com>
2023-12-22 22:19:52 +0100

    font change

--- lib/src/theme/textSchemes.dart
+++ lib/src/theme/textSchemes.dart
@@ -4,98 +4,97 @@ const blackTextSchemes = TextTheme(
   displayLarge: TextStyle(
     color: Color.fromARGB(255, 255, 255, 255),
     fontWeight: FontWeight.w600,
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     fontSize: 57,
 
   ),
   displayMedium: TextStyle(
-    
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     color: Color.fromARGB(255, 255, 255, 255),
     fontWeight: FontWeight.w600,
     fontSize: 48,
 
   ),
   displaySmall: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     color: Color.fromARGB(255, 255, 255, 255),
     fontWeight: FontWeight.w600,
     fontSize: 36,
 
   ),
   headlineLarge: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     fontWeight: FontWeight.w600,
     fontSize: 32,
 
   ),
   headlineMedium: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     fontWeight: FontWeight.w400,
     fontSize: 28,
 
   ),
   headlineSmall: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     fontWeight: FontWeight.w400,
     fontSize: 20,
 
   ),
   titleLarge: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Display",
     fontWeight: FontWeight.w700,
     fontSize: 22,
 
   ),
   titleMedium: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     fontWeight: FontWeight.w500,
     fontSize: 16,
 
   ),
   titleSmall: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     fontWeight: FontWeight.w500,
     fontSize: 14,
 
   ),
   labelLarge: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     color: Color.fromARGB(255, 255, 255, 255),
     fontWeight: FontWeight.w300,
-    fontSize: 18,
+    fontSize: 16,
 
   ),
   labelMedium: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     color: Color.fromARGB(255, 255, 255, 255),
     fontWeight: FontWeight.w300,
     fontSize: 14,
 
   ),
   labelSmall: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     color: Color.fromARGB(255, 255, 255, 255),
     fontWeight: FontWeight.w200,
     fontSize: 11,
 
   ),
   bodyLarge: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     color: Color.fromARGB(255, 168, 168, 168),
     fontWeight: FontWeight.w400,
     fontSize: 18,
   
   ),
   bodyMedium: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     color: Color.fromARGB(255, 168, 168, 168),
     fontWeight: FontWeight.w400,
     fontSize: 14,
  
   ),
   bodySmall: TextStyle(
-    fontFamily: "SF Pro",
+    fontFamily: "SF Pro Text",
     color: Color.fromARGB(255, 168, 168, 168),
     fontWeight: FontWeight.w400,
     fontSize: 11,

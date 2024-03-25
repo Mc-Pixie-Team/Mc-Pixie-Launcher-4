21b75196018cf237726da61f2492cd36b1ef9722
joshiGaming <88989206+joshiGaming@users.noreply.github.com>
2024-03-25 20:35:47 +0100

    cef intergration windows

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:mclauncher4/src/tasks/auth/supabase.dart';
 import 'package:mclauncher4/src/tasks/utils/path.dart';
-import 'package:webview_cef/webview_cef.dart';
+
 
 void main() async{
   supabaseHelpers().init();
   DiscordRP().initCS("1144740158374158366");
    Paint.enableDithering = true;
   await Path.init();
-  await WebViewGlobal.controller.initialize();
   runApp(
        McLauncher());
     final win = appWindow;
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -7,7 +7,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   dart_discord_rpc
   desktop_drop
   flutter_secure_storage_linux
-  gtk
   url_launcher_linux
   webview_cef
 )
